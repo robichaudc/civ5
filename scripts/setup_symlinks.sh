e10dac9581ec529ced4fcbd741ea388e012920f4
robi <crobicha@gmail.com>
2022-07-02 07:51:16 -0400

    pocatello

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -1,9 +1,12 @@
 set -x
-SAVE_DIR=~/".local/share/Aspyr/Sid Meier's Civilization 5/Saves/single/quick"
+#SAVE_DIR=~/".local/share/Aspyr/Sid Meier's Civilization 5/Saves/single/quick"
+SAVE_DIR="~/Library/Application\ Support/Sid\ Meier?s\ Civilization\ 5/Saves/single/quick"
+
 cd ..
 REPO_DIR=`pwd`
 
-cd "$SAVE_DIR"
+#cd "${SAVE_DIR}"
+cd ~/Library/Application\ Support/Sid\ Meier\'s\ Civilization\ 5/Saves/single/quick
 mv QuickSave.Civ5Save QuickSave.Civ5Save.old
 ln -s $REPO_DIR/saves/QuickSave.Civ5Save
 
